ba5508b2b67012dc4e9e1486f078fc599b14ec4a
Murtaza Oklu <murtazaoklu1993@gmail.com>
2017-10-02 20:16:02 -0500

    Add files via upload
    Calculate fee of worked for hours and minute

---
+++ a format parameter used as a local variable/NewClass1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MUSA
+ */
+public class NewClass1 {
+    public static void main(String[] args){
+        System.out.println("Welcome");
+        NewClass yourBill = new NewClass();
+        yourBill.inputTimeWorked();
+        yourBill.updateFee();
+        yourBill.outputBill();
+        System.out.println("dasdasda");
+        System.out.println("ogsldgmk");
+    }
+}
